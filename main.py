b465138d12189cfec0a7dac7f4f2a171dfaaf762
jamesgotch24-lab <jamesgotch24@gmail.com>
2026-09-07 06:07:19 +0000

    commit 1.32

--- main.py
+++ main.py
@@ -568,4 +568,3 @@ async def websocket_terminal_endpoint(websocket: WebSocket):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-    
